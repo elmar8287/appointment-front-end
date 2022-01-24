aab271bd30e34e2cc5f3ef79bd87b3a52e427869
Elmar <L-mar@inbox.ru>
2022-01-25 00:58:57 +0400

    Fix eslint errors

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Landing from '../landing/landing';
 
 const App = () => {
-    return (
-        <div>
-            <Landing />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Landing />
+    </div>
+  );
+};
 
 export default App;
