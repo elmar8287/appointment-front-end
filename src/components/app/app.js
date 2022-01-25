f187deb3435246d590ea3f07b4f1248aef4f96d7
Elmar <L-mar@inbox.ru>
2022-01-25 11:57:40 +0400

    Fix linters

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Route, Routes, BrowserRouter} from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Landing from '../landing/landing';
 import Login from '../login/login';
 
 const App = () => (
-  
+
   <BrowserRouter>
-    
     <Routes>
-      <Route exact path='/' element={<Landing />} />
-      <Route path='login' element={<Login />} />
+      <Route exact path="/" element={<Landing />} />
+      <Route path="login" element={<Login />} />
     </Routes>    
   </BrowserRouter>
 );
