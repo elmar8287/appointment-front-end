4a7d251c0f2f86c579dc87551d8cf24ccf9dbe83
Elmar <L-mar@inbox.ru>
2022-02-01 20:30:04 +0400

    Fix login process

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,30 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import '../landing/landing.css'
 import Partners from '../partners/partners';
+import { authenticateUser } from '../../store/users-reducer';
 
 const hero2 = require('./img/hero2.png');
 
+
+
 const Login = () => {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.userReducer);
+
+  useEffect(() => {
+    if (user != null) { navigate('/'); }
+  }, [user]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch(authenticateUser(username));
+  }
+
+
   return (
     <div>
       <section className="section-hero">
       <div className="container-hero">
         <div className="hero__text-box">
-          <form className="form form--login" action="">
+          <form className="form form--login" onSubmit={handleSubmit}>
             <div>
-              <label htmlFor="email">Email Address</label>
+              <label htmlFor="username">Username</label>
               <input
-                type="email"
-                name="email"
+                type="text"
+                name="username"
                 id="email"
-                placeholder="Enter your email"
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Enter your username"
                 required
               />
             </div>
             <br />
             <div>
-              <label htmlFor="password">Pasword</label>
+              <label htmlFor="password">Password</label>
               <input
                 type="password"
                 name="password"
@@ -34,9 +54,9 @@ const Login = () => {
               />
             </div>
             <br />
-            <Link to="/main">
-              <a className="btn btn--full margin-top--sm" href="#">Log In</a>
-            </Link>
+            
+            <input type="submit" value="Login" className="btn btn--full margin-top--sm" />
+            
             
           </form>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store/configureStore';
 import App from './components/app/app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
